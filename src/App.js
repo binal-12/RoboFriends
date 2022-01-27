4d705f720b2643e32fec7a30a382d188311c8a91
binal-12 <binalchhodvadiya12@gmail.com>
2022-01-27 22:45:21 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,28 @@ import 'tachyons'
 import './App.css'
 
 class App extends Component{
+
+    constructor(){
+        super()
+        this.state = {
+            'robots' : robots,
+            'searchfield' : ""
+        } 
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ 'searchfield' : event.target.value })
+    }
+    
     render(){
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
         return(
             <div className='tc'>
                 <h1 className="f1 m3 p3">RoboFriends</h1>
-                <Searchbar />
-                <Cardlist robots={robots}/>
+                <Searchbar onSearch={this.onSearchChange}/>
+                <Cardlist robots={filteredRobots}/>
             </div>
         )
     }
--- src/Searchbar.js
+++ src/Searchbar.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-const Searchbar = () => {
+const Searchbar = ({onSearch}) => {
     return(
-        <input type="search" className="input-reset ba dib b--black-20 pa2 mb2 db w-10" placeholder="Search robots" />
+        <input 
+            type="search" 
+            className="input-reset bg-lightest-blue b-light-green ba dib b--black-20 pa2 mb2 db w-20 br3" 
+            placeholder="Search robots"
+            onChange={onSearch}
+        />
     )
 }
 
